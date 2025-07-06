fc3738df4338b53aabea41cb363af2b57a03d19e
zuhaib786 <suhaibulsaman@gmail.com>
2025-07-06 14:14:00 +0530

    feat: version 3. 1.25x speed up (10s -> 8s)

---
+++ cs336_basics/byte_pair_tokenizer_inverted_index.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+from typing import Dict, Set
+
+from cs336_basics.byte_pair_encoding_tokenizer import BytePairEncodingTokenizer
+from cs336_basics.utils import *
+
+MAX_NUM_CHUNKS = 100  # Spawn maximum 100 processes
+
+"""
+Implementation of BPE tokenizer.
+"""
+
+
+class BytePairEncodingTokenizerInvertedIndex(BytePairEncodingTokenizer):
+    def __init__(self, special_tokens: List[str] = None):
+        super().__init__(special_tokens)
+        self.token_inverted_index: Dict[int, Set[int]] = defaultdict(set)
+        self.pre_tokens_inverted_index: List[Tuple[int, ...]] = []
+
+    def generate_byte_pair_index(self) -> None:
+        self.pre_tokens_inverted_index = list(self.pre_tokens_dict.keys())
+        for word_split, val in self.pre_tokens_dict.items():
+            for tok1, tok2 in zip(word_split[:-1], word_split[1:]):
+                self.byte_pair_index[(tok1, tok2)] = (
+                    self.byte_pair_index.get((tok1, tok2), 0) + val
+                )
+        for idx, word_split in enumerate(self.pre_tokens_inverted_index):
+            for tok in word_split:
+                self.token_inverted_index[tok].add(idx)
+
+    def _update_byte_pair_index(self, max_pair: Tuple[int, int]) -> None:
+        pre_tokens_index_list = list(
+            self.token_inverted_index[max_pair[0]].union(
+                self.token_inverted_index[max_pair[1]]
+            )
+        )
+        pre_tokens = [
+            self.pre_tokens_inverted_index[idx] for idx in pre_tokens_index_list
+        ]
+        vocab_len = len(self._vocab)
+        for pre_token_index, word_split in zip(pre_tokens_index_list, pre_tokens):
+            if max_pair[0] not in word_split:
+                continue
+            idx = 0
+            word_split_len, new_word_split_len = len(word_split), 0
+            new_word_split = [0] * word_split_len
+            while idx < word_split_len - 1:
+                if (
+                    word_split[idx] == max_pair[0]
+                    and word_split[idx + 1] == max_pair[1]
+                ):
+                    new_word_split[new_word_split_len] = vocab_len
+                    new_word_split_len += 1
+                    idx += 2
+                    continue
+                new_word_split[new_word_split_len] = word_split[idx]
+                new_word_split_len += 1
+                idx += 1
+            if idx == word_split_len - 1:
+                new_word_split[new_word_split_len] = word_split[idx]
+                new_word_split_len += 1
+            if new_word_split_len == word_split_len:
+                continue
+            # Update pre tokens dict
+            val = self.pre_tokens_dict.pop(word_split)
+            new_word_split = new_word_split[:new_word_split_len]
+            self.pre_tokens_inverted_index[pre_token_index] = tuple(new_word_split)
+            self.pre_tokens_dict[tuple(new_word_split)] = val
+            # Update the byte pair frequency index
+            for tok1, tok2 in zip(word_split[:-1], word_split[1:]):
+                self.byte_pair_index[(tok1, tok2)] -= val
+            for tok1, tok2 in zip(new_word_split[:-1], new_word_split[1:]):
+                self.byte_pair_index[(tok1, tok2)] = (
+                    self.byte_pair_index.get((tok1, tok2), 0) + val
+                )
+            for tok in new_word_split:
+                self.token_inverted_index[tok].add(pre_token_index)
